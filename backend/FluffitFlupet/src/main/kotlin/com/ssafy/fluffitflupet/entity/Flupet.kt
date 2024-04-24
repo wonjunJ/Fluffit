eb36290de08c5e59e3872913d88afb70ff4bb36d
정원준 <museoul@naver.com>
2024-04-24 17:47:38 +0900

    엔티티 초기 세팅

---
+++ backend/FluffitFlupet/src/main/kotlin/com/ssafy/fluffitflupet/entity/Flupet.kt
@@ -0,0 +1,14 @@
+package com.ssafy.fluffitflupet.entity
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Table
+
+@Table("flupet")
+class Flupet(
+    @Id
+    val id: Long? = null,
+    var name: String,
+    var imgUrl: String,
+    var stage: Int
+) {
+}
---
+++ backend/FluffitFlupet/src/main/kotlin/com/ssafy/fluffitflupet/entity/FoodType.kt
@@ -0,0 +1,17 @@
+package com.ssafy.fluffitflupet.entity
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Table
+
+@Table("food_type")
+class FoodType(
+    @Id
+    val id: Long? = null,
+    var name: String,
+    var imgUrl: String,
+    var fullnessEffect: Int,
+    var health_effect: Int,
+    var price: Int,
+    var description: String
+) {
+}
