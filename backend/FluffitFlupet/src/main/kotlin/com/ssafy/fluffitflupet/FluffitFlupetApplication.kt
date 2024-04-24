0ea1374ed1da5077f3a18a2a3c0a71b49256f837
정원준 <museoul@naver.com>
2024-04-24 13:38:54 +0900

    추가 세팅 완료

--- backend/FluffitFlupet/src/main/kotlin/com/ssafy/fluffitflupet/FluffitFlupetApplication.kt
+++ backend/FluffitFlupet/src/main/kotlin/com/ssafy/fluffitflupet/FluffitFlupetApplication.kt
@@ -1,11 +1,23 @@
 package com.ssafy.fluffitflupet
 
+import feign.Logger
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.context.annotation.Bean
+import reactor.util.Loggers
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 class FluffitFlupetApplication
 
 fun main(args: Array<String>) {
     runApplication<FluffitFlupetApplication>(*args)
+}
+
+@Bean
+fun feignLoggerLevel(): Logger.Level {
+    return Logger.Level.FULL
 }
