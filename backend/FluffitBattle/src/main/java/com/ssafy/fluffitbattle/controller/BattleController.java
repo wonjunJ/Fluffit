c167f17204fd4556fcb9483eca9054b57a8296ab
serot <serotonins023@gmail.com>
2024-05-02 13:16:32 +0900

    chore: 기초 코드 작성

--- backend/FluffitBattle/src/main/java/com/ssafy/fluffitbattle/controller/BattleController.java
+++ backend/FluffitBattle/src/main/java/com/ssafy/fluffitbattle/controller/BattleController.java
@@ -1,2 +1,16 @@
-package com.ssafy.fluffitbattle.controller;public class BattleController {
+package com.ssafy.fluffitbattle.controller;
+
+import com.ssafy.fluffitbattle.service.BattleService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/battle")
+public class BattleController {
+
+    private final BattleService battleService;
 }
