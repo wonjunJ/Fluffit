20b4fccb98cd4f5a5105debcc76b0994c63d5e13
금예인 <tbc07061@naver.com>
2024-05-02 12:36:35 +0900

    chore: 프로젝트 구조 수정

--- backend/FluffitMember/src/main/java/com/ssafy/fluffitmember/FluffitmemberApplication.java
+++ backend/FluffitMember/src/main/java/com/ssafy/fluffitmember/FluffitmemberApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FluffitmemberApplication {
+public class  FluffitmemberApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FluffitmemberApplication.class, args);
---
+++ backend/FluffitMember/src/main/java/com/ssafy/fluffitmember/appstatus/AppStatusController.java
@@ -0,0 +1,9 @@
+package com.ssafy.fluffitmember.appstatus;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/app-status")
+public class AppStatusController {
+}
