0e33e982d7b989421f32893ee651ef0c6b606de8
정원준 <museoul@naver.com>
2024-05-03 17:59:51 +0900

    [Feat]Eureka 프로젝트 생성

---
+++ backend/FluffitEureka/src/test/java/com/ssafy/eurekaservice/FluffitEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluffitEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
