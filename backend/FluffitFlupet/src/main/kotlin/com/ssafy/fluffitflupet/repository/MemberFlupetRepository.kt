69fcece90b97b337181f6cb60f0a591d866fb16f
이준서 <wnstj7866@naver.com>
2024-05-05 23:42:34 +0900

    Deleted backend/FluffitFlupet/gradle/wrapper/gradle-wrapper.jar, backend/FluffitFlupet/gradle/wrapper/gradle-wrapper.properties, backend/FluffitFlupet/src/main/kotlin/com/ssafy/fluffitflupet/config/R2dbcConfig.kt, backend/FluffitFlupet/src/main/kotlin/com/ssafy/fluffitflupet/controller/FlupetController.kt, backend/FluffitFlupet/src/main/kotlin/com/ssafy/fluffitflupet/dto/MainInfoResponse.kt, backend/FluffitFlupet/src/main/kotlin/com/ssafy/fluffitflupet/entity/Flupet.kt, backend/FluffitFlupet/src/main/kotlin/com/ssafy/fluffitflupet/entity/FoodType.kt, backend/FluffitFlupet/src/main/kotlin/com/ssafy/fluffitflupet/entity/MemberFlupet.kt, backend/FluffitFlupet/src/main/kotlin/com/ssafy/fluffitflupet/repository/MemberFlupetRepository.kt, backend/FluffitFlupet/src/main/kotlin/com/ssafy/fluffitflupet/service/FlupetService.kt, backend/FluffitFlupet/src/main/kotlin/com/ssafy/fluffitflupet/FluffitFlupetApplication.kt, backend/FluffitFlupet/src/main/resources/application.yml, backend/FluffitFlupet/src/main/resources/bootstrap.yml, backend/FluffitFlupet/src/main/resources/schema.sql, backend/FluffitFlupet/src/test/kotlin/com/ssafy/fluffitflupet/FluffitFlupetApplicationTests.kt, backend/FluffitFlupet/.gitignore, backend/FluffitFlupet/build.gradle, backend/FluffitFlupet/gradlew, backend/FluffitFlupet/gradlew.bat, backend/FluffitFlupet/settings.gradle, backend/FluffitMember/gradle/wrapper/gradle-wrapper.jar, backend/FluffitMember/gradle/wrapper/gradle-wrapper.properties, backend/FluffitMember/src/main/java/com/ssafy/fluffitmember/appstatus/AppStatusController.java, backend/FluffitMember/src/main/java/com/ssafy/fluffitmember/member/MemberController.java, backend/FluffitMember/src/main/java/com/ssafy/fluffitmember/FluffitmemberApplication.java, backend/FluffitMember/src/main/resources/application.properties, backend/FluffitMember/src/test/java/com/ssafy/fluffitmember/FluffitmemberApplicationTests.java, backend/FluffitMember/.gitignore, backend/FluffitMember/build.gradle, backend/FluffitMember/gradlew, backend/FluffitMember/gradlew.bat, backend/FluffitMember/settings.gradle, backend/FluffitConfig/gradle/wrapper/gradle-wrapper.jar, backend/FluffitConfig/gradle/wrapper/gradle-wrapper.properties, backend/FluffitConfig/src/main/java/com/ssafy/configservice/config/JasyptConfig.java, backend/FluffitConfig/src/main/java/com/ssafy/configservice/ConfigServiceApplication.java, backend/FluffitConfig/src/main/resources/application.yml, backend/FluffitConfig/src/main/resources/bootstrap.yml, backend/FluffitConfig/src/test/java/com/ssafy/configservice/ConfigServiceApplicationTests.java, backend/FluffitConfig/.gitignore, backend/FluffitConfig/apiEncryptionKey.jks, backend/FluffitConfig/build.gradle, backend/FluffitConfig/gradlew, backend/FluffitConfig/gradlew.bat, backend/FluffitConfig/settings.gradle, backend/FluffitBattle/gradle/wrapper/gradle-wrapper.jar, backend/FluffitBattle/gradle/wrapper/gradle-wrapper.properties, backend/FluffitBattle/src/main/java/com/ssafy/fluffitbattle/config/RedisConfig.java, backend/FluffitBattle/src/main/java/com/ssafy/fluffitbattle/controller/BattleController.java, backend/FluffitBattle/src/main/java/com/ssafy/fluffitbattle/entity/dto/BattleResponseDto.java, backend/FluffitBattle/src/main/java/com/ssafy/fluffitbattle/entity/Battle.java, backend/FluffitBattle/src/main/java/com/ssafy/fluffitbattle/entity/BattleType.java, backend/FluffitBattle/src/main/java/com/ssafy/fluffitbattle/repository/BattleRepository.java, backend/FluffitBattle/src/main/java/com/ssafy/fluffitbattle/service/BattleService.java, backend/FluffitBattle/src/main/java/com/ssafy/fluffitbattle/FluffitbattleApplication.java, backend/FluffitBattle/src/main/resources/application.properties, backend/FluffitBattle/src/test/java/com/ssafy/fluffitbattle/FluffitbattleApplicationTests.java, backend/FluffitBattle/.gitignore, backend/FluffitBattle/build.gradle, backend/FluffitBattle/gradlew, backend/FluffitBattle/gradlew.bat, backend/FluffitBattle/settings.gradle

--- backend/FluffitFlupet/src/main/kotlin/com/ssafy/fluffitflupet/repository/MemberFlupetRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.ssafy.fluffitflupet.repository
-
-import com.ssafy.fluffitflupet.entity.MemberFlupet
-import org.springframework.data.repository.kotlin.CoroutineCrudRepository
-
-interface MemberFlupetRepository: CoroutineCrudRepository<MemberFlupet, Long> {
-
-}
