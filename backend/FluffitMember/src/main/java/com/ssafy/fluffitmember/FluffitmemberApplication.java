0888fa1bb1fb02c2bf87dc941d26684156f9f24b
wnstj7866 <wnstj7866@naver.com>
2024-05-07 08:47:52 +0900

    feat : config 의존을 사용하기 위한 설정 추가

--- backend/FluffitMember/src/main/java/com/ssafy/fluffitmember/FluffitmemberApplication.java
+++ backend/FluffitMember/src/main/java/com/ssafy/fluffitmember/FluffitmemberApplication.java
@@ -2,8 +2,10 @@ package com.ssafy.fluffitmember;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class  FluffitmemberApplication {
 
 	public static void main(String[] args) {
