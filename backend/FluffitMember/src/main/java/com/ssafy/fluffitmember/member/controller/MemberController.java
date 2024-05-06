ca477c6d714f92bfc0c34c5237c97389bbdd51db
wnstj7866 <wnstj7866@naver.com>
2024-05-06 19:45:41 +0900

    fix : 패키지 위치 이동

--- backend/FluffitMember/src/main/java/com/ssafy/fluffitmember/member/controller/MemberController.java
+++ backend/FluffitMember/src/main/java/com/ssafy/fluffitmember/member/controller/MemberController.java
@@ -1,4 +1,4 @@
-package com.ssafy.fluffitmember.member;
+package com.ssafy.fluffitmember.member.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
