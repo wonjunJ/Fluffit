6edcb73981bd29c8d22c7789772b7e483e028fad
이준서 <wnstj7866@naver.com>
2024-05-06 14:55:10 +0900

    feat : AuthController, AuthService 생성

---
+++ backend/FluffitMember/src/main/java/com/ssafy/fluffitmember/auth/controller/AuthController.java
@@ -0,0 +1,25 @@
+package com.ssafy.fluffitmember.auth.controller;
+
+import com.ssafy.fluffitmember._common.response.success.SuccessResponse;
+import com.ssafy.fluffitmember.auth.dto.request.LoginReqDto;
+import com.ssafy.fluffitmember.auth.dto.response.LoginResDto;
+import com.ssafy.fluffitmember.auth.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public SuccessResponse<LoginResDto> login(@RequestBody LoginReqDto loginReqDto){
+        a
+    }
+
+}
---
+++ backend/FluffitMember/src/main/java/com/ssafy/fluffitmember/auth/service/AuthService.java
@@ -0,0 +1,7 @@
+package com.ssafy.fluffitmember.auth.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+}
