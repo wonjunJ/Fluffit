44eb86b6fc8e38de7cc7375a1c9b9a755b17bec2
wnstj7866 <wnstj7866@naver.com>
2024-05-06 19:45:20 +0900

    fix : 필드 추가

--- backend/FluffitMember/src/main/java/com/ssafy/fluffitmember/auth/dto/request/LoginReqDto.java
+++ backend/FluffitMember/src/main/java/com/ssafy/fluffitmember/auth/dto/request/LoginReqDto.java
@@ -5,16 +5,20 @@ import lombok.Getter;
 
 @Getter
 public class LoginReqDto {
-    private final String signature;
+
+    private final String userCode; // 사용자의 naver id
+    private final String signature; // 암호화된 id
 
     @Builder
-    private LoginReqDto(String signature){
+    private LoginReqDto(String userCode,String signature){
+        this.userCode = userCode;
         this.signature = signature;
     }
 
-    public static LoginReqDto of(String signature){
+    public static LoginReqDto of(String userCode,String signature){
         return builder()
+                .userCode(userCode)
                 .signature(signature)
                 .build();
-    }
+    }   
 }
