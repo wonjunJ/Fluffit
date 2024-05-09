57d0ad5341ccf8de9d09b7b1c1841828b058ac26
wnstj7866 <wnstj7866@naver.com>
2024-05-09 13:28:18 +0900

    feat : exception 추가

---
+++ backend/FluffitMember/src/main/java/com/ssafy/fluffitmember/exception/NotFoundUserException.java
@@ -0,0 +1,4 @@
+package com.ssafy.fluffitmember.exception;
+
+public class NotFoundUserException extends RuntimeException {
+}
---
+++ backend/FluffitMember/src/main/java/com/ssafy/fluffitmember/exception/NotValidNickname.java
@@ -0,0 +1,5 @@
+package com.ssafy.fluffitmember.exception;
+
+public class NotValidNickname extends RuntimeException {
+
+}
