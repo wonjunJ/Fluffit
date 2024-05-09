e261e5c0ac2c631045119a05c16788a391def441
wnstj7866 <wnstj7866@naver.com>
2024-05-09 14:06:32 +0900

    feat : EnableJpaAuditing관련 어노테이션 추가 (CreatedDate)

--- backend/FluffitMember/src/main/java/com/ssafy/fluffitmember/FluffitmemberApplication.java
+++ backend/FluffitMember/src/main/java/com/ssafy/fluffitmember/FluffitmemberApplication.java
@@ -3,9 +3,11 @@ package com.ssafy.fluffitmember;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableJpaAuditing
 public class  FluffitmemberApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FluffitmemberApplication.class, args);
--- backend/FluffitMember/src/main/java/com/ssafy/fluffitmember/member/entity/Member.java
+++ backend/FluffitMember/src/main/java/com/ssafy/fluffitmember/member/entity/Member.java
@@ -3,6 +3,7 @@ package com.ssafy.fluffitmember.member.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -12,10 +13,12 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 public class Member {
 
     @Id
