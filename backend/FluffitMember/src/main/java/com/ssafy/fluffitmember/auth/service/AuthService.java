71c80c44472f178112a793da4f8b5a872fa9f8d8
wnstj7866 <wnstj7866@naver.com>
2024-05-10 16:14:47 +0900

    fix : toString 삭제, log 하나 삭제

--- backend/FluffitMember/src/main/java/com/ssafy/fluffitmember/auth/service/AuthService.java
+++ backend/FluffitMember/src/main/java/com/ssafy/fluffitmember/auth/service/AuthService.java
@@ -91,8 +91,7 @@ public class AuthService {
         // 데이터를 암호화
         byte[] result = mac.doFinal(data.getBytes(StandardCharsets.UTF_8));
 
-        String encodeData = Base64.getEncoder().encodeToString(result).toString();
-        log.info("encodeData = " + encodeData);
+        String encodeData = Base64.getEncoder().encodeToString(result);
         log.info("encodeData = " + encodeData);
         // 바이너리 데이터를 Base64로 인코딩하여 문자열로 반환
         return encodeData;
