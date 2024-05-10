955d6a8789f109f72b884732bfca6ad26f285872
wnstj7866 <wnstj7866@naver.com>
2024-05-10 13:39:48 +0900

    feat : 저장된 리프레시 토큰과 다를 때 에러타입 추가

--- backend/FluffitMember/src/main/java/com/ssafy/fluffitmember/error/ErrorStateCode.java
+++ backend/FluffitMember/src/main/java/com/ssafy/fluffitmember/error/ErrorStateCode.java
@@ -9,11 +9,12 @@ public enum ErrorStateCode {
 
     UNAUTHORIXED("401", "권한이 없습니다"),
     ACCESS_TOKEN_EXPIRE("403", "ACCESS 토큰 만료"),
-    REFRESH_TOKEN_EXPIRE("401", "리프레시 토큰 만료"),
     REDIS("408", "캐시 서버에 문제가 있습니다"),
     RUNTIME("500", "서버 에러"),
+    REFRESH_TOKEN_EXPIRE("401", "리프레시 토큰 만료"),
     TOKEN_INVALID("400", "토큰 형식이 잘못되었습니다"),
     TOKEN_UNAUTH("402", "토큰이 일치하지 않습니다"),
+    NOT_VALID_REFRESHTOKEN("400", "저장된 리프레시 토큰과 다릅니다"),
     GATEWAY("503", "게이트 웨이 요청에 문제가 있습니다"),
     SUCCESS("200", "성공"),
     NO_SUCH_ALGORITHM("4001","요청된 암호화 알고리즘이 시스템에서 지원되지 않거나 존재하지 않습니다."),
---
+++ backend/FluffitMember/src/main/java/com/ssafy/fluffitmember/exception/NotValidRefreshToken.java
@@ -0,0 +1,5 @@
+package com.ssafy.fluffitmember.exception;
+
+public class NotValidRefreshToken extends RuntimeException {
+
+}
