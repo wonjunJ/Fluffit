6860a12015418891b7d6b6e13071531a21cd5de5
wnstj7866 <wnstj7866@naver.com>
2024-05-10 14:01:53 +0900

    fix : member dto 파일 형식 변경

--- backend/FluffitMember/src/main/java/com/ssafy/fluffitmember/member/dto/Response/GetNicknameResDto.java
+++ backend/FluffitMember/src/main/java/com/ssafy/fluffitmember/member/dto/Response/GetNicknameResDto.java
@@ -1,4 +1,4 @@
-package com.ssafy.fluffitmember.member.dto;
+package com.ssafy.fluffitmember.member.dto.Response;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,6 +7,6 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-public class UpdateNicknameReqDto {
+public class GetNicknameResDto {
     private String nickname;
 }
