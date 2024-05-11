8a3fc3d194104464ef5fbefe414ae7c1eb8b6414
정원준 <museoul@naver.com>
2024-05-11 23:11:15 +0900

    [Fix] 인게임 정보 조회시 jooq쿼리문 수정 및 스케쥴러 1분간격으로 임시 수정

--- backend/FluffitFlupet/src/main/kotlin/com/ssafy/fluffitflupet/client/MemberServiceClientAsync.kt
+++ backend/FluffitFlupet/src/main/kotlin/com/ssafy/fluffitflupet/client/MemberServiceClientAsync.kt
@@ -9,6 +9,12 @@ class MemberServiceClientAsync(
     private val client: MemberServiceClient
 ) {
     suspend fun getUserCoin(userId: String): Int = withContext(Dispatchers.IO) {
-        client.getUserCoin(userId)
+        try {
+            println("feign client 요청은 보냈지??")
+            return@withContext client.getUserCoin(userId)
+        }catch (e: Exception){
+            println("feign client 오류 코드는 ${e.printStackTrace()}")
+            return@withContext -1
+        }
     }
 }
