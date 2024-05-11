077610321e6a79b9eb877818079688e7e192a2e6
이준서 <wnstj7866@naver.com>
2024-05-11 20:00:20 +0900

    fix : ACCESS_TOKEN_EXPIRE_TIME_IN_MILLISECONDS 수정 (일주일)

--- backend/FluffitMember/src/main/java/com/ssafy/fluffitmember/jwt/JwtUtil.java
+++ backend/FluffitMember/src/main/java/com/ssafy/fluffitmember/jwt/JwtUtil.java
@@ -20,7 +20,7 @@ public class JwtUtil {
     private final JwtTokenService jwtTokenService;
 
     private static final long REFRESH_TOKEN_EXPIRE_TIME_IN_MILLISECONDS = 1000L * 60L * 60L * 24L * 14; //refresh token 2week
-    private static final long ACCESS_TOKEN_EXPIRE_TIME_IN_MILLISECONDS = 1000 * 60 * 60; //access token 30min
+    private static final long ACCESS_TOKEN_EXPIRE_TIME_IN_MILLISECONDS = 1000 * 60 * 60 * 24 * 7; //access token 30min
 
     private final Environment env;
     private Key key;
