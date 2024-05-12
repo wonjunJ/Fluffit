6a47ec88ac162c6a15bc48f231f0c4fcc8b64f72
이준서 <wnstj7866@naver.com>
2024-05-13 02:46:23 +0900

    feat : StepsRepository @Query 문 추가

--- backend/FluffitMember/src/main/java/com/ssafy/fluffitmember/exercise/repository/StepsRepository.java
+++ backend/FluffitMember/src/main/java/com/ssafy/fluffitmember/exercise/repository/StepsRepository.java
@@ -2,6 +2,7 @@ package com.ssafy.fluffitmember.exercise.repository;
 
 import com.ssafy.fluffitmember.exercise.entity.Steps;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -9,5 +10,6 @@ import java.util.Optional;
 
 @Repository
 public interface StepsRepository extends JpaRepository<Steps,Integer> {
+    @Query("SELECT s FROM Steps s WHERE s.member.id = :memberId AND s.date = :date")
     Optional<Steps> findByMemberIdAndDate(String memberId, LocalDate date);
 }
