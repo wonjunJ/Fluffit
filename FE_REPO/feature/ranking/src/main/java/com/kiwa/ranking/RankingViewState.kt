3cccca68a6bc90fe36620670520861b0b15faf29
정수현 <gcjung777@gmail.com>
2024-05-13 14:33:13 +0900

    feat: ranking snackbar
    랭킹 스낵바 구현

--- FE_REPO/feature/ranking/src/main/java/com/kiwa/ranking/RankingViewState.kt
+++ FE_REPO/feature/ranking/src/main/java/com/kiwa/ranking/RankingViewState.kt
@@ -4,16 +4,19 @@ import com.kiwa.fluffit.base.ViewState
 import com.kiwa.fluffit.model.ranking.RankingInfo
 
 sealed class RankingViewState : ViewState {
+    abstract val message: String
 
     data class AgeRanking(
         val ageRankingList: List<RankingInfo> = emptyList(),
-        val myRanking: RankingInfo = RankingInfo(0, "", "", "", "")
+        val myRanking: RankingInfo = RankingInfo(0, "", "", "", ""),
+        override val message: String = ""
     ) :
         RankingViewState()
 
     data class BattleRanking(
         val battleRankingList: List<RankingInfo> = emptyList(),
-        val myRanking: RankingInfo = RankingInfo(0, "", "", "", "")
+        val myRanking: RankingInfo = RankingInfo(0, "", "", "", ""),
+        override val message: String = ""
     ) :
         RankingViewState()
 }
