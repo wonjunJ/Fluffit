94b30f5b08d0ccef0f88f3ae6766cb14d32a6eef
정원준 <museoul@naver.com>
2024-05-14 19:57:37 +0900

    [Feat] kafka 설정 추가

--- backend/FluffitFlupet/src/main/kotlin/com/ssafy/fluffitflupet/config/kafkaconfig/KafkaConsumerConfig.kt
+++ backend/FluffitFlupet/src/main/kotlin/com/ssafy/fluffitflupet/config/kafkaconfig/KafkaConsumerConfig.kt
@@ -1,33 +1,33 @@
 package com.ssafy.fluffitflupet.config.kafkaconfig
 
-//import org.apache.kafka.clients.consumer.ConsumerConfig
-//import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.common.serialization.StringDeserializer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-//import org.springframework.kafka.annotation.EnableKafka
-//import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
-//import org.springframework.kafka.core.ConsumerFactory
-//import org.springframework.kafka.core.DefaultKafkaConsumerFactory
+import org.springframework.kafka.annotation.EnableKafka
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
+import org.springframework.kafka.core.ConsumerFactory
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory
 
-//@EnableKafka
+@EnableKafka
 @Configuration
 class KafkaConsumerConfig {
     //접속할 수 있는 kafka의 정보
-//    @Bean
-//    fun consumerFactory(): ConsumerFactory<String, String> {
-//        var properties = HashMap<String, Any>()
-//        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "fluffit.shop:9092")
-//        //consumer들을 grouping하기 위한 것(여러개의 consumer가 데이터를 가져갈때 전달하고자 하는 group을 지정하여 정보를 보낼 수 있다.)
-//        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "kafka-fluffit-group")
-//        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer::class.java)
-//        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer::class.java)
-//        return DefaultKafkaConsumerFactory(properties)
-//    }
-//
-//    @Bean
-//    fun kafkaListenerContainerFactory(): ConcurrentKafkaListenerContainerFactory<String, String> {
-//        val factory = ConcurrentKafkaListenerContainerFactory<String, String>()
-//        factory.consumerFactory = consumerFactory()
-//        return factory
-//    }
+    @Bean
+    fun consumerFactory(): ConsumerFactory<String, String> {
+        var properties = HashMap<String, Any>()
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092")
+        //consumer들을 grouping하기 위한 것(여러개의 consumer가 데이터를 가져갈때 전달하고자 하는 group을 지정하여 정보를 보낼 수 있다.)
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "consumerGroupId")
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer::class.java)
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer::class.java)
+        return DefaultKafkaConsumerFactory(properties)
+    }
+
+    @Bean
+    fun kafkaListenerContainerFactory(): ConcurrentKafkaListenerContainerFactory<String, String> {
+        val factory = ConcurrentKafkaListenerContainerFactory<String, String>()
+        factory.consumerFactory = consumerFactory()
+        return factory
+    }
 }
--- backend/FluffitFlupet/src/main/kotlin/com/ssafy/fluffitflupet/messagequeue/KafkaConsumer.kt
+++ backend/FluffitFlupet/src/main/kotlin/com/ssafy/fluffitflupet/messagequeue/KafkaConsumer.kt
@@ -2,20 +2,25 @@ package com.ssafy.fluffitflupet.messagequeue
 
 import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.ObjectMapper
-//import org.springframework.kafka.annotation.KafkaListener
+import com.ssafy.fluffitflupet.service.FlupetService
+import org.slf4j.LoggerFactory
+import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Service
 
 @Service
 class KafkaConsumer {
-//    @KafkaListener(topics = ["ex_temp"])
-//    fun update(message: String) {
-//        //var map = HashMap<Any, Any>()
-//        val mapper = ObjectMapper()
-//        val map: Map<Any, Any> = try {
-//            mapper.readValue(message, object : TypeReference<Map<Any, Any>>() {})
-//        } catch (ex: Exception) {
-//            ex.printStackTrace()
-//            return
-//        }
-//    }
+    private val log = LoggerFactory.getLogger(FlupetService::class.java)
+
+    @KafkaListener(topics = ["steps-update"], groupId = "consumerGroupId")
+    fun update(message: String) {
+        log.info("카프카로부터 제대로 메세지를 받나?? [$message]")
+        //var map = HashMap<Any, Any>()
+        val mapper = ObjectMapper()
+        val map: Map<Any, Any> = try {
+            mapper.readValue(message, object : TypeReference<Map<Any, Any>>() {})
+        } catch (ex: Exception) {
+            ex.printStackTrace()
+            return
+        }
+    }
 }
