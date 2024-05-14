7a1e5831d885407f3c2454da2e13829aeb82a98a
이준서 <wnstj7866@naver.com>
2024-05-14 18:48:20 +0900

    fix : 카프카 설정 수정 kafka:9092

--- backend/FluffitMember/src/main/java/com/ssafy/fluffitmember/messagequeue/KafkaConsumerConfig.java
+++ backend/FluffitMember/src/main/java/com/ssafy/fluffitmember/messagequeue/KafkaConsumerConfig.java
@@ -22,7 +22,7 @@ public class KafkaConsumerConfig {
     @Bean
     public ConsumerFactory<String, String> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "43.201.50.244:29092");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "consumerGroupId");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
