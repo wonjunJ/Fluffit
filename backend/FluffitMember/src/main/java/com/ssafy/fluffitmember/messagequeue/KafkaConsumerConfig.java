002aa46df4298dc78b069030cf92dd2fbc640c6d
wnstj7866 <wnstj7866@naver.com>
2024-05-17 13:46:27 +0900

    feat : ENABLE_AUTO_COMMIT_CONFIG 자동 커밋 활성화

--- backend/FluffitMember/src/main/java/com/ssafy/fluffitmember/messagequeue/KafkaConsumerConfig.java
+++ backend/FluffitMember/src/main/java/com/ssafy/fluffitmember/messagequeue/KafkaConsumerConfig.java
@@ -26,7 +26,8 @@ public class KafkaConsumerConfig {
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "consumerGroupId");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,"latest");  // 설정 변경
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest"); // 혹은 "none"으로 설정
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true); // 자동 커밋 활성화
         return new DefaultKafkaConsumerFactory<>(props);
     }
 
