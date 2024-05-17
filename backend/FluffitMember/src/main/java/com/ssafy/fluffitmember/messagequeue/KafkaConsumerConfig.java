1486a21b71259bb8d71514cad0cb46b0f4229935
wnstj7866 <wnstj7866@naver.com>
2024-05-17 13:50:53 +0900

    feat : Boolean.TRUE true 레퍼클래스로 변경

--- backend/FluffitMember/src/main/java/com/ssafy/fluffitmember/messagequeue/KafkaConsumerConfig.java
+++ backend/FluffitMember/src/main/java/com/ssafy/fluffitmember/messagequeue/KafkaConsumerConfig.java
@@ -27,7 +27,7 @@ public class KafkaConsumerConfig {
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest"); // 혹은 "none"으로 설정
-        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true); // 자동 커밋 활성화
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, Boolean.TRUE); // 자동 커밋 활성화
         return new DefaultKafkaConsumerFactory<>(props);
     }
 
