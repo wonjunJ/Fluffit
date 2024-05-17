f6f910311a67a85c8f6b84149a7e972d1204ee0f
serot <serotonins023@gmail.com>
2024-05-17 10:46:16 +0900

    chore: statistics non null

--- backend/FluffitBattle/src/main/java/com/ssafy/fluffitbattle/repository/BattleRepository.java
+++ backend/FluffitBattle/src/main/java/com/ssafy/fluffitbattle/repository/BattleRepository.java
@@ -17,9 +17,11 @@ public interface BattleRepository extends JpaRepository<Battle, Long> {
 //    Optional<Battle> findByOrganizerIdOrParticipantId(Long id);
     Slice<Battle> findByOrganizerIdOrParticipantIdOrderByBattleDateDesc(String organizerId, String participantId, Pageable pageable);
 
-    @Query("SELECT new com.ssafy.fluffitbattle.entity.dto.BattleStatisticItemDto(b.battleType, COUNT(b), SUM(CASE WHEN b.winnerId = :userId THEN 1 ELSE 0 END), " +
+    @Query("SELECT new com.ssafy.fluffitbattle.entity.dto.BattleStatisticItemDto(b.battleType, COUNT(b), " +
+            "SUM(CASE WHEN b.winnerId = :userId THEN 1 ELSE 0 END), " +
             "SUM(CASE WHEN b.winnerId != :userId THEN 1 ELSE 0 END)) " +
             "FROM Battle b WHERE b.organizerId = :userId OR b.participantId = :userId " +
-            "GROUP BY b.battleType")
+            "GROUP BY b.battleType " +
+            "HAVING COUNT(b) > 0")
     List<BattleStatisticItemDto> findBattleStatsByUserId(String userId);
 }
